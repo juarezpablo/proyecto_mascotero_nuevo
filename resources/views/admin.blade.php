b96f8c2c3340313ebb68ae43853df381805245e6
Juan Pablo Juarez <juarezjpablo7@gmail.com>
2021-12-12 17:46:27 -0300

    agregadolinkbootstr

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Admin</title>
 </head>
 <body>
    <h1>Bienvenido {{ session('alias') }}  </h1> 
@@ -14,7 +15,7 @@ Nombre de Admin es {{ session('alias') }} y su password es {{ session('pass') }}
 
 
 
-<a href="{{ route('admin.conseguir_usuarios') }}" id="lista_usuarios">Ver lista de Usuarios  </a>
+<a href="{{ route('admin.conseguir_usuarios') }}" id="lista_usuarios"  class="btn btn-success">Ver lista de Usuarios  </a>
 
 
 </body>
--- resources/views/admin_usuarios.blade.php
+++ resources/views/admin_usuarios.blade.php
@@ -1,28 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Gestion de Usuarios</title>
+</head>
+<body>
 
-Vista de lista de usuarios
-
-@foreach ($Tabla as $usuario)
-<table>
-     <thead>
-         <tr>
-             <th>Alias</th>
-             <th>Contraseña</th>
-             <th>Nombre</th>
-             <th>Apellido</th>
-                 
-         </tr>
-     </thead>
-     <tbody>
-     <tr>
-         <td>{{ $usuario->alias }}</td>
-         <td>{{ $usuario->contrasena }}</td>
-         <td>{{ $usuario->nombre }}</td>
-         <td>{{ $usuario->apellido }}</td>
+    <h1>Vista de lista de usuarios</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Alias</th>
+                <th>Contraseña</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                    
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($Tabla as $usuario)
+            <tr>
+                 <td>{{ $usuario->alias }}</td>
+                <td>{{ $usuario->contrasena }}</td>
+                 <td>{{ $usuario->nombre }}</td>
+                <td>{{ $usuario->apellido }}</td>
+            </tr>
      
-     </tr>
-    </tbody>
-</table>
-<a href="" class="btn btn-success">Link</a>
-<br><br><br>    
-@endforeach
+            @endforeach
+
+        </tbody>
+    </table>
+    <a href="" class=""></a>
+    <br><br><br>    
+</body>
+</html>
+
+
+
 
